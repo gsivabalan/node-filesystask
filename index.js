127865b2b7b123296ca2e054d5ed1f6a6b5df9d0
Sivabalan G <122631523+gsivabalan@users.noreply.github.com>
2023-05-25 17:28:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post("/api/create-text-file", (req, res) => {
-  // const folder = "files";
-  // const file = `${folder}/current-date-time.txt`;
+  const folder = "files";
+  const file = `${folder}/current-date-time.txt`;
 
-  // if (!folder) {
-  //   throw new Error("Please specify a folder");
-  // }
+  if (!folder) {
+    throw new Error("Please specify a folder");
+  }
 
-  // if (!fs.existsSync(folder)) {
-  //   throw new Error("Folder does not exist");
-  // }
+  if (!fs.existsSync(folder)) {
+    throw new Error("Folder does not exist");
+  }
 
   const istDateTime = new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" });
 
